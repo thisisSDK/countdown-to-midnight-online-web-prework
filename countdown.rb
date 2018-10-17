0bb5b31bb7efad6ed9119e000241c79ad8d06693
sdk1270 <sdk1270@gmail.com>
2018-10-17 16:02:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+  
+def countdown(countdown_output)
+  countdown_output = 10
+  while countdown_output > 0 
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1 
+  end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(countdown_output)
+  countdown_output = 10
+  while countdown_output > 0 
+    puts "#{countdown_output} SECOND(S)!"
+    sleep(1)
+    countdown_output -= 1 
+  end
+    "HAPPY NEW YEAR!"
 end
